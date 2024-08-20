a22c5fe481abe0b2b81b1a64cf24b5e0d922b156
latoma <127959432+latoma@users.noreply.github.com>
2024-08-20 16:29:02 +0300

    1.7. done

--- Osa 1/Ex 1.6-1.11 unicafe/src/App.jsx
+++ Osa 1/Ex 1.6-1.11 unicafe/src/App.jsx
@@ -2,28 +2,26 @@ import { useState } from 'react'
 
 const Title = ({text}) => <h1>{text.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}</h1>
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
+
+const AverageFeedback = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  return (
+    <p>Average: {(good - bad) / total}</p>
+  )
+}
+
+const PositiveFeedback = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  return (
+    <p>Positive: {(good / total) * 100}%</p>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -42,18 +40,19 @@ const App = () => {
     setBad(bad + 1)
   }
 
-
   return (
     <div>
       <Title text='give feedback' />
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
+
       <Title text='statistics' />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-
+      <AverageFeedback good={good} neutral={neutral} bad={bad} />
+      <PositiveFeedback good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
