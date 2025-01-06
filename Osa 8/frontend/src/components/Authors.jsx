0a19abeddbfe19055ca43118c8c4b65d93d079cf
latoma <127959432+latoma@users.noreply.github.com>
2025-01-06 12:03:40 +0200

    8.8-8.9 done

--- Osa 8/frontend/src/components/Authors.jsx
+++ Osa 8/frontend/src/components/Authors.jsx
@@ -1,8 +1,16 @@
+import { useQuery } from "@apollo/client"
+import { ALL_AUTHORS } from "../queries"
+
 const Authors = (props) => {
-  if (!props.show) {
+  const result = useQuery(ALL_AUTHORS)
+
+  if(!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -14,7 +22,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {result.data.allAuthors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
